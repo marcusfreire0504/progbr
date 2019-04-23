057e80b606967f4e6fd505d07e0ccece9755c3b8
Nidio Dolfini <nidiosdolfini@gmail.com>
2019-04-23 18:01:27 -0300

    update js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-var a = "1";
-var b = 1;
-console.log(1 + 1);
-console.log("1" + "1");
-=======
-var a = 3;
-var b = 3;
+var idade = 17;
 
-console.log('erer " asd "asds ');
- 
-console.log(a === 3);
->>>>>>> f4157f66c791619068e1864f61b82cd995064e90
+if(idade >= 18){
+    console.log("pode");
+    console.log("qual seu pedido");
+}
+else{
+    console.log("não pode");
+}
