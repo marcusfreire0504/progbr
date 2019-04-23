d1ad76fd2906e8cee4bf0fc5d9fafa2f02cd89e3
Nídio Dolfini <nidiosdolfini@gmail.com>
2019-04-23 20:54:45 -0300

    arrays and for

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 var idade = 17;
 
-if(idade >= 18){
+if(idade){
     console.log("pode");
     console.log("qual seu pedido");
 }
 else{
     console.log("não pode");
 }
+
+var numero = 10;
+
+for (var vez = 0; vez <= numero; vez++){
+    console.log("volta no for " + vez);
+}
+console.log("acabou for");
+
+var alunos = new Array("nidio", "joão", "herbert");
+
+for(var aluno in alunos)
+    console.log(aluno);
+for(var aluno of alunos)
+    console.log(aluno);
+
