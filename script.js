b69a0096f0a0770f01e8a36a7070940eb4e8f37b
Nidio Dolfini <nidiosdolfini@gmail.com>
2019-04-24 18:18:19 -0300

    contrutores

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-function calcmedia(){
-    return (this.notas[0], this.notas[1]) /2
+function aluno(nome, nota1, nota2) {
+    this.nome = nome;
+    this.nota1 = nota1;
+    this.nota2 = nota2;
+
+    this.media = function(){
+        return (this.nota1 + this.nota2) / 2;
+    }
 }
+var aluno1 = new aluno("Nidio", 8 ,7);
 
-var aluno = {
-    nome: "Nidio",
-    notas: [7.5, 5.0 ],
-
-    media: calcmedia,
-}
-
-console.log(aluno.nome);
-console.log(aluno.media());
+console.log(aluno1.nome + " - " + aluno1.media())
