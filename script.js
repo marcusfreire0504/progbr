970e4453751e7c5503c01afe31bbf21f3c1fd979
Nidio Dolfini <nidiosdolfini@gmail.com>
2019-04-24 17:52:41 -0300

    metodos

--- script.js
+++ script.js
@@ -1,31 +1,13 @@
-var nomes = ["Nidio", "João", "Herbert" ]
-var notaA = [7, 8.1, 9.5]
-var notaB = [8, 1, 5]
-
-function media(n1, n2) {
-    return (n1 + n2) /2
+function calcmedia(){
+    return (this.notas[0], this.notas[1]) /2
 }
 
-function passou(media) {
-    if (media > 7) {
-        return "Aprovado"
-    } else {
-        return "Reprovado"
-    }
+var aluno = {
+    nome: "Nidio",
+    notas: [7.5, 5.0 ],
+
+    media: calcmedia,
 }
 
-for (var index in nomes) {
-    var nota1 = notaA[index]
-    var nota2 = notaB[index]
-    var m = media(nota1, nota2)
-
-    console.log(nomes[index] +
-        " - " +
-        nota1 +
-        " - " +
-        nota2 +
-        " - " +
-        passou(m)
-    );
-    
-}
+console.log(aluno.nome);
+console.log(aluno.media());
