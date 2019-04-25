f7c3d85093a30ff30b073345613373f63e889e86
Nidio Dolfini <nidiosdolfini@gmail.com>
2019-04-25 17:40:44 -0300

    resumo objetos

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function aluno(nome, nota1, nota2) {
+//criando um objeto e colocando dentro de uma variavel
+var objtVariael = {
+    nome: "Nidio", sobrenome: "Dolfini" 
+}
+
+console.log(objtVariael.nome)
+
+//criando um objeto com funcao precisa de new para criar o objeto
+function objtFuncao(nome, nota1, nota2) {
     this.nome = nome;
     this.nota1 = nota1;
     this.nota2 = nota2;
@@ -7,6 +15,8 @@ function aluno(nome, nota1, nota2) {
         return (this.nota1 + this.nota2) / 2;
     }
 }
-var aluno1 = new aluno("Nidio", 8 ,7);
+var aluno1 = new objtFuncao("Nidio", 8 ,7);
 
 console.log(aluno1.nome + " - " + aluno1.media())
+
+console.log(aluno1)
