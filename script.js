5aa37bd0527fab9bc9578a02d8c110428af98ef5
Nidio Dolfini <nidiosdolfini@gmail.com>
2019-04-26 12:26:09 -0300

    teste array e data

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ var data = new Date()
 data.toLocaleDateString
 console.log(data.toLocaleDateString());
 console.log(data.toDateString());
+console.log(data.toTimeString());
+
+
+var a = ["Nidio", "Herbert", "João","Nidio", "Herbert", "João"]
+
+console.log(a[1])
+
+a.forEach(element => {
+    console.log(element)
+});
