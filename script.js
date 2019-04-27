f71e72832ef77ba671157356f7a45b5151b2847b
Nídio Dolfini <nidiosdolfini@gmail.com>
2019-04-26 21:09:21 -0300

    fim JS basico

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-var data = new Date()
+{
+const a = 4;
+let b = 5; //let só é acessivel dento do escopo
+var c = 6; // é uma variavel constante, não pde reatribuir e é de escopo
+}
 
-data.toLocaleDateString
-console.log(data.toLocaleDateString());
-console.log(data.toDateString());
-console.log(data.toTimeString());
+console.log(a);
+console.log(b);
+console.log(c);
 
-
-var a = ["Nidio", "Herbert", "João","Nidio", "Herbert", "João"]
-
-console.log(a[1])
-
-a.forEach(element => {
-    console.log(element)
-});
