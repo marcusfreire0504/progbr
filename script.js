feaedf331f91a7748843f20b7c1720323d663a59
Nídio Dolfini <nidiosdolfini@gmail.com>
2019-04-27 20:16:04 -0300

    aula document

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-{
-const a = 4;
-let b = 5; //let só é acessivel dento do escopo
-var c = 6; // é uma variavel constante, não pde reatribuir e é de escopo
-}
-
-console.log(a);
-console.log(b);
-console.log(c);
-
+console.log(document)
