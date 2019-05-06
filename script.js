5579a5d882b39a6e4bafb4043991ca8021250372
Nidio Dolfini <nidiosdolfini@gmail.com>
2019-05-06 17:59:03 -0300

    canvas

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 let tela = document.getElementById("tela");
 
 let ctx = tela.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(250, 250);
-ctx.lineTo(500, 0);
-ctx.strokeStyle = "blue";
-ctx.stroke();
+let x = 250;
+let y = 250;
+let raio = 100;
+
+let inicio = 0;
+let fim = 2 * Math.PI;
+
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+
+ctx.arc(x, y, raio, inicio, fim);
+ctx.fill();
+ctx.stroke()
+
